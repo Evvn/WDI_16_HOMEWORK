52a08ec0dab54fbc59c1a29d6f01d65825d44a20
Evan <evvn@outlook.com>
2018-08-06 17:02:06 +1000

    commit 4/5/6/ accidentally sorry i didnt realise i was skipping things

--- Evan/wk08/1-mon/pixel_art/app.js
+++ Evan/wk08/1-mon/pixel_art/app.js
@@ -3,6 +3,7 @@ console.log('Pixel');
 var currentColor = $('.current_color')
 var colorForm = $('.set_color')
 var colorInput = $('.color_input')
+var square = $('.square')
 
 function setColor() {
   event.preventDefault()
@@ -10,3 +11,6 @@ function setColor() {
 }
 
 colorForm.on('submit', setColor)
+square.on('mouseover', function(event) {
+  $(event.target).css('backgroundColor', currentColor.css('backgroundColor'))
+})
